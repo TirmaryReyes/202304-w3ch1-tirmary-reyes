0f99ac1b15ba57eb071a464383bac53cd04ea8b9
Tirmary Reyes <tmrv1991@gmail.com>
2023-04-18 21:55:06 +0200

    Add specifications of each character

--- app/js/index.js
+++ app/js/index.js
@@ -2,7 +2,7 @@ import characters from "./characters/characters.js";
 import CharacterList from "./components/CharacterList/CharacterList.js";
 import Component from "./components/Component/Component.js";
 
-const selector = document.querySelector(".app-container");
+const selector = document.querySelector(".app");
 
 const text = new Component(selector, "div", "hola");
 const characterList = new CharacterList(selector, characters);
