ddbbccc36511348d3162fd56877852ef90030465
Tirmary Reyes <tmrv1991@gmail.com>
2023-04-18 21:12:05 +0200

    Add age in characyerCard

--- app/js/components/CharacterCard/CharacterCard.js
+++ app/js/components/CharacterCard/CharacterCard.js
@@ -16,7 +16,7 @@ class CharacterCard extends Component {
             <h2 class="character__name card-title h4">${this.character.characterData.name} ${this.character.characterData.family}</h2>
             <div class="character__info">
               <ul class="list-unstyled">
-                <li>Age: X yrs</li>
+                <li>${this.character.characterData.age}</li>
                 <li>
                   State:
                   <i class="fas fa-thumbs-down"></i>
